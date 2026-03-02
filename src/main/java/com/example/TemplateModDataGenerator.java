217ca07e0dc10384624dbee07e694bfa76dc0b06
Miami <ft2k@proton.me>
2026-03-03 00:33:55 +0100

    ?

--- src/main/java/com/example/TemplateModDataGenerator.java
+++ src/main/java/com/example/TemplateModDataGenerator.java
@@ -1,11 +1,14 @@
 package com.example;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
+@Environment(EnvType.CLIENT)
 public class TemplateModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 	}
 }
